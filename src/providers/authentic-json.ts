9b427171b0ca3f7a0da0b01b7de45914cd670deb
Kevin McCann <kevin.j.mccann@gmail.com>
2017-02-28 16:00:05 -0500

    updated imports on authentic-json

--- src/providers/authentic-json.ts
+++ src/providers/authentic-json.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Jsonp, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthenticJSON {
 
-  data: any;
+	public jsonp:any;
+    data: any;
 
-  constructor(public http: Http) {
-  
+  constructor(jsonp: Jsonp, public http: Http) {
+    this.jsonp = jsonp;
   }
 
   load(name:String, password:String) {
     if (this.data) {
         return Promise.resolve(this.data);
     }
+    
       let params = new URLSearchParams('callback=JSONP_CALLBACK');
       params.set('term', keyword);
-      return this.jsonp.request('localhost:4200/send', {
+      return this.jsonp.request('localhost:8080/send', {
         search: params
       }).toPromise()
         .then((response) => response.json().results);
